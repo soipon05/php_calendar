c3b0de1c96aee9a3ec83cc7e9717d642fb89e2cd
JUN <soipon0@gmail.com>
2019-05-02 23:16:33 +0900

    カレンダーの雛形作成

--- public/index.php
+++ public/index.php
@@ -1,3 +1,59 @@
 <?php
 
 echo "Hello World ";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calendar</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th><a href="">&laquo;</a></th>
+                <th colspan="5">August 2015</th>
+                <th><a href="">&raquo;</a></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Sun</td>
+                <td>Mon</td>
+                <td>Tue</td>
+                <td>Web</td>
+                <td>Thu</td>
+                <td>Fri</td>
+                <td>Sat</td>
+            </tr>
+            <tr>
+                <td class="youbi_0">1</td>
+                <td class="youbi_1">2</td>
+                <td class="youbi_2">3</td>
+                <td class="youbi_3">4</td>
+                <td class="youbi_4 today">5</td>
+                <td class="youbi_5">6</td>
+                <td class="youbi_6">7</td>
+            </tr>
+            <tr>
+                <td class="youbi0">30</td>
+                <td class="youbi1">31</td>
+                <td class="gray">1</td>
+                <td class="gray">2</td>
+                <td class="gray">3</td>
+                <td class="gray">4</td>
+                <td class="gray">5</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="7"><a href="">Today</a></th>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+</html>
